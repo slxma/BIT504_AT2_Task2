e70aeca86278bc8e4521af751f9cb637762273a4
slxma <147021225+slxma@users.noreply.github.com>
2024-05-05 18:19:22 +1200

    Updated Board.java
    Finished the TODOs on the board.java

--- BIT504_Assign2_PartAStudent/Board.java
+++ BIT504_Assign2_PartAStudent/Board.java
@@ -12,7 +12,8 @@ public class Board {
 	/** Constructor to create the game board */
 	public Board() {
 		
-	 //TODO: initialise the cells array using ROWS and COLS constants 
+	 //TODO: initialise the cells array using ROWS and COLS constants
+	 cells = new Cell[GameMain.ROWS][GameMain.COLS];
 
 		
 		for (int row = 0; row < GameMain.ROWS; ++row) {
@@ -29,10 +30,16 @@ public class Board {
 		// TODO: Check whether the game has ended in a draw. 
 		// Hint: Use a nested loop (see the constructor for an example). Check whether any of the cells content in the board grid are Player.Empty. If they are, it is not a draw.
 		// Hint: Return false if it is not a draw, return true if there are no empty positions left
-		   
-		
 
-		
+		for (int row = 0; row < GameMain.ROWS; ++row) {
+			for (int col = 0; col < GameMain.COLS; ++col) {
+				if (cells[row][col].content == Player.Empty) {
+					return false;
+				}
+			}
+		}
+		return true;
+				
 	}
 	
 	/** Return true if the current player "thePlayer" has won after making their move  */
@@ -42,8 +49,10 @@ public class Board {
 			return true; 
 		
 		 // TODO: Check if the player has 3 in the playerCol.
-		 // Hint: Use the row code above as a starting point, remember that it goes cells[row][column] 
-		
+		 // Hint: Use the row code above as a starting point, remember that it goes cells[row][column]
+		 if (cells[0][playerCol].content == thePlayer && cells[1][playerCol].content == thePlayer && cells[2][playerCol].content == thePlayer) {
+			return true;
+		 }		
 		
 		
 		 // 3-in-the-diagonal
@@ -52,6 +61,9 @@ public class Board {
 		 
 		
 		// TODO: Check the diagonal in the other direction
+		if (cells[0][2].content == thePlayer && cells[1][1].content == thePlayer && cells[2][0].content == thePlayer) {
+			return true;
+		}
 		
 
 		
